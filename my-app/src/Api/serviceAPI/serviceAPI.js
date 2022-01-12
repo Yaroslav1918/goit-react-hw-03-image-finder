a1c45f0ba98400a4189c541c090dbc8453b90d4b
Yaroslav Shtanko <81259415+Yaroslav1918@users.noreply.github.com>
2022-01-12 23:21:55 +0200

    bag-fix

--- my-app/src/Api/serviceAPI/serviceAPI.js
+++ my-app/src/Api/serviceAPI/serviceAPI.js
@@ -1,4 +1,4 @@
-function fetchImg(name, page = null) {
+function fetchImg(name, page) {
   const apiKey = "24120553-f8b1f79ab38ca933100336740";
   const BASE_URL = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${name}&page=${page}&per_page=12&key=${apiKey}`;
   return fetch(BASE_URL).then((response) => {
--- my-app/src/components/App/App.js
+++ my-app/src/components/App/App.js
@@ -1,25 +1,111 @@
-import React, { Component } from "react";
+import { Component } from "react";
+import ImageGallery from "../ImageGallery";
+import serviceAPI from "../../Api/serviceAPI/serviceAPI";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Button from "../Button";
+import Modal from "../Modal";
+import { ModalImg } from "../Modal/Modal.styled";
+import Loader from "../Loader";
+import Searchbar from "../Searchbar";
 import GlobalStyle from "../../Style/globalStyles";
-import SearchBar from "../Searchbar";
-import ImageGalleryInfo from "../ImageGalleryInfo";
 
-class App extends Component {
+const Status = {
+  IDLE: "idle",
+  PENDING: "pending",
+  RESOLVED: "resolved",
+  REJECTED: "rejected",
+};
+
+export default class App extends Component {
   state = {
+    gallery: [],
+    error: null,
+    status: Status.IDLE,
+    urlImg: "",
+    page: 1,
+    showModal: false,
     seacrhQuery: "",
   };
+
   handleFormSubmit = (query) => {
-    this.setState({ seacrhQuery: query });
+    this.setState({ seacrhQuery: query, page: 1, gallery: [] });
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { seacrhQuery, page } = this.state;
+    if (prevState.seacrhQuery !== seacrhQuery || prevState.page !== page) {
+      this.setState({ status: Status.PENDING });
+      this.fetchGallary();
+    }
+  }
+  fetchGallary = () => {
+    const { page, seacrhQuery } = this.state;
+
+    serviceAPI
+      .fetchImg(seacrhQuery, page)
+      .then(({ hits }) => {
+        this.setState((prevState) => ({
+          gallery: [...prevState.gallery, ...hits],
+          page,
+          status: Status.RESOLVED,
+        }));
+        if (page !== 1) this.scrollPage();
+      })
+
+      .catch((error) => this.setState({ error, status: Status.REJECTED }));
+  };
+
+  onClickImageURL = (urlImg) => {
+    this.setState({ urlImg });
+    this.toggleModal();
+  };
+  scrollPage = () => {
+    window.scrollTo({
+      top: document.documentElement.scrollHeight,
+      behavior: "smooth",
+    });
+  };
+
+  onLoadMore = () => {
+    this.setState({ status: Status.PENDING });
+    this.setState({ page: this.state.page + 1 });
+  };
+  toggleModal = () => {
+    this.setState(({ showModal }) => ({
+      showModal: !showModal,
+    }));
   };
 
   render() {
+    const { gallery, status, showModal, urlImg, eror } = this.state;
+
     return (
       <>
         <GlobalStyle />
-        <SearchBar onSubmit={this.handleFormSubmit} />
-        <ImageGalleryInfo valueName={this.state.seacrhQuery} />
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        {status === "idle" && null}
+
+        {status === "pending" && <Loader />}
+
+        {status === "rejected" && <div>{eror}</div>}
+
+        {status === "resolved" && (
+          <>
+            <ToastContainer />
+
+            <ImageGallery hits={gallery} onClick={this.onClickImageURL} />
+            {gallery.length > 0 ? (
+              <Button onLoadMore={this.onLoadMore} />
+            ) : null}
+            {showModal && (
+              <Modal onClose={this.toggleModal}>
+                <ModalImg src={urlImg} />
+              </Modal>
+            )}
+          </>
+        )}
       </>
     );
   }
 }
-
-export default App;
